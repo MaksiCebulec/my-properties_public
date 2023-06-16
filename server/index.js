98108a0d8e55cef7e7411e17f89f2a109a35eba2
Maksi <maksi.cebulec@gmail.com>
2023-06-16 15:13:01 +0200

    3rd commit: connected to database, added properties route. Tested with postman

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 ///middleware
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+
+//Routes
+//get all properties
+app.get("/properties", async (req, res) => {
+    try {
+        const properties = await pool.query("SELECT * FROM properties");
+        res.json(properties.rows);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
+
+
 
 app.listen(5000, () => {
     console.log("Server has stated (port 5000).");
