5a79e2f86a7af1505318cadb218f25da7e1fea45
Maksi <maksi.cebulec@gmail.com>
2023-06-18 19:47:00 +0200

    15th commit added styling for mobile

--- client/src/components/pagination/pagination.component.jsx
+++ client/src/components/pagination/pagination.component.jsx
@@ -1,15 +1,38 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import './pagination.styles.scss';
 
 
 const Pagination = ({ setCurrentPage, currentPage, getProperties, totalPages, totalProperties }) => {
 
 
+    const [pageRange, setPageRange] = useState([]);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        setIsMobile(isMobileCheck());
+
+    }, []);
+
+    function isMobileCheck() {
+        const screenWidth = window.innerWidth;
+        console.log("Screen width: ", screenWidth);
+
+        if (screenWidth < 768) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
 
 
     function getPageRange() {
+
         const minPage = Math.max(currentPage - 2, 1);
         const maxPage = Math.min(currentPage + 2, totalPages);
+
 
         // return Array.from({ length: maxPage - minPage + 1 }, (_, index) => minPage + index);
 
@@ -23,6 +46,8 @@ const Pagination = ({ setCurrentPage, currentPage, getProperties, totalPages, to
 
         return pages;
     }
+
+
 
 
     function nextPage() {
@@ -44,13 +69,26 @@ const Pagination = ({ setCurrentPage, currentPage, getProperties, totalPages, to
         // setCurrentPage(page); //it already do this in getProperties
     }
 
-    const pages = getPageRange();
+    useEffect(() => {
+        setPageRange(getPageRange());
+    }, [currentPage, totalPages]);
+
+    // const pages = getPageRange();
 
     return (
         <Fragment>
             <div className="pagination">
+                {isMobile ? (
+                    <div className="arrow-row">
+                        <button onClick={nextPage} className={`pagination-button next-page-button ${currentPage === totalPages ? 'hidden' : ''}`}>Previous page</button>
+                        <button onClick={nextPage} className={`pagination-button prev-page-button ${currentPage === totalPages ? 'hidden' : ''}`}>Next Page</button>
+                    </div>
+                ) : (
+                    ''
+                )}
+
                 <ul className="pagination-list">
-                    <li className={`pagination-item ${currentPage === 1 ? 'hidden' : ''}`}>
+                    <li className={`pagination-item ${currentPage === 1 || isMobile ? 'hidden' : ''}`}>
                         <button href="" onClick={previousPage} className="pagination-button">&laquo;</button>
                     </li>
                     <li className={`pagination-item`}>
@@ -63,9 +101,9 @@ const Pagination = ({ setCurrentPage, currentPage, getProperties, totalPages, to
                     ) : (
                         ''
                     )}
-                    {pages.map((page) => {
+                    {pageRange.map((page) => {
                         return (<li key={page} className="pagination-item">
-                            <button onClick={() => { onClick(page) }} className={`pagination-button ${currentPage === page ? 'active' : ''}`}>{page}</button>
+                            <button href="#h1" onClick={() => { onClick(page) }} className={`pagination-button ${currentPage === page ? 'active' : ''}`}>{page}</button>
                         </li>)
                     })}
                     {currentPage < (totalPages - 4) ? (
@@ -78,11 +116,12 @@ const Pagination = ({ setCurrentPage, currentPage, getProperties, totalPages, to
                     <li className={`pagination-item`}>
                         <button href="" onClick={() => onClick(totalPages)} className={`pagination-button ${currentPage === totalPages ? 'active' : ''}`}>{totalPages}</button>
                     </li>
-                    <li className={`pagination-item ${currentPage === totalPages ? 'hidden' : ''}`}>
+                    <li className={`pagination-item ${currentPage === totalPages || isMobile ? 'hidden' : ''}`}>
                         <button onClick={nextPage} className="pagination-button">&raquo;</button>
                     </li>
 
                 </ul>
+
             </div>
             <h4>Showing {(currentPage * 21) - 20} - {currentPage * 21} of {totalProperties} properties.</h4>
         </Fragment >
